d54e495ad902fe56bb06ac0e945f772c075f8113
Ben-97-0 <B.Norell@share.epsb.ca>
2024-10-09 14:33:38 -0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,116 @@
+let totalCost = 0;
+document.getElementById("but4").addEventListener("click", clickedSmoothie);
+
+function clickedSmoothie() {
+  totalCost += 3;
+  let output = `$${totalCost}.00`;
+  document.getElementById("total").innerHTML = output;
+}
+document.getElementById("but2").addEventListener("click", clickedjuice);
+function clickedjuice() {
+  totalCost += 2;
+  let output = `$${totalCost}.00`;
+  document.getElementById("total").innerHTML = output;
+}
+document.getElementById("but1").addEventListener("click", clickedsoda);
+function clickedsoda() {
+  totalCost += 1;
+  let output = `$${totalCost}.00`;
+  document.getElementById("total").innerHTML = output;
+}
+document.getElementById("but2").addEventListener("click", clickedjuice);
+function clickedjuice() {
+  totalCost += 2;
+  let output = `$${totalCost}.00`;
+  document.getElementById("total").innerHTML = output;
+}
+document.getElementById("bur").addEventListener("click", clickedchicken);
+function clickedchicken() {
+  totalCost += 7;
+  let output = `$${totalCost}.00`;
+  document.getElementById("total").innerHTML = output;
+}
+document.getElementById("bur2").addEventListener("click", clickedspice);
+function clickedspice() {
+  totalCost += 8;
+  let output = `$${totalCost}.00`;
+  document.getElementById("total").innerHTML = output;
+}
+document.getElementById("bur3").addEventListener("click", clickedcheese);
+function clickedcheese() {
+  totalCost += 6;
+  let output = `$${totalCost}.00`;
+  document.getElementById("total").innerHTML = output;
+}
+document.getElementById("bur4").addEventListener("click", clickeddouble);
+function clickeddouble() {
+  totalCost += 8;
+  let output = `$${totalCost}.00`;
+  document.getElementById("total").innerHTML = output;
+}
+document.getElementById("sal").addEventListener("click", clickedsalad);
+function clickedsalad() {
+  totalCost += 5;
+  let output = `$${totalCost}.00`;
+  document.getElementById("total").innerHTML = output;
+}
+document.getElementById("fry").addEventListener("click", clickedfry);
+function clickedfry() {
+  totalCost += 4;
+  let output = `$${totalCost}.00`;
+  document.getElementById("total").innerHTML = output;
+}
+document.getElementById("wing").addEventListener("click", clickedwing);
+function clickedwing() {
+  totalCost += 8;
+  let output = `$${totalCost}.00`;
+  document.getElementById("total").innerHTML = output;
+}
+document.getElementById("bru").addEventListener("click", clickedbru);
+function clickedbru() {
+  totalCost += 7;
+  let output = `$${totalCost}.00`;
+  document.getElementById("total").innerHTML = output;
+}
+document.getElementById("crab").addEventListener("click", clickedcrab);
+function clickedcrab() {
+  totalCost += 5;
+  let output = `$${totalCost}.00`;
+  document.getElementById("total").innerHTML = output;
+}
+document.getElementById("buff").addEventListener("click", clickedbuff);
+function clickedbuff() {
+  totalCost += 8;
+  let output = `$${totalCost}.00`;
+  document.getElementById("total").innerHTML = output;
+}
+document.getElementById("nch").addEventListener("click", clickednch);
+function clickednch() {
+  totalCost += 6;
+  let output = `$${totalCost}.00`;
+  document.getElementById("total").innerHTML = output;
+}
+document.getElementById("per").addEventListener("click", clickedper);
+function clickedper() {
+  totalCost += 7;
+  let output = `$${totalCost}.00`;
+  document.getElementById("total").innerHTML = output;
+}
+document.getElementById("sma").addEventListener("click", clickedsma);
+function clickedsma() {
+  totalCost += 10;
+  let output = `$${totalCost}.00`;
+  document.getElementById("total").innerHTML = output;
+}
+document.getElementById("med").addEventListener("click", clickedmed);
+function clickedmed() {
+  totalCost += 12;
+  let output = `$${totalCost}.00`;
+  document.getElementById("total").innerHTML = output;
+}
+document.getElementById("lar").addEventListener("click", clickedlar);
+function clickedlar() {
+  totalCost += 15;
+  let output = `$${totalCost}.00`;
+  document.getElementById("total").innerHTML = output;
+}
